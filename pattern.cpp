03e3f199d70c5284c592b45742a6be73c6a1ea47
sonalee59 <128119003+sonalee59@users.noreply.github.com>
2023-03-17 12:05:10 +0530

    learned * pattern

---
+++ pattern.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std; 
+//left side * pattern
+int main ()
+{
+    int n=5;
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+        }   
+        return 0;
+
+}
+
+
+
+
+#include<iostream>
+using namespace std;
+      // left side reverse * pattern
+int main()
+{
+    int n=5;
+    for(int i=n;i>=0;i--){
+        for(int j=0;j<=i;j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+            
+        }
+    
+        return 0;
+    }
+    
+    
+
+    
+    
+    
+    
+    
+    
+   
